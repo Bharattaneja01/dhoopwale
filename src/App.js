775d76813225080b896d2cc1e02d644c949c1c0f
Bharat Taneja <113545688+Bharattaneja01@users.noreply.github.com>
2025-01-18 22:28:55 +0530

    Did some work on product detail page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './customers/components/Navigation/Navigation';
 import HomePage from './customers/pages/HomePage/HomePage';
 import Footer from './customers/components/Footer/Footer';
 import Product from './customers/components/Product/Product';
+import ProductDetails from './customers/components/ProductDetails/ProductDetails';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Navigation />
       <div>
         {/* <HomePage /> */}
-        <Product />
+        {/* <Product /> */}
+        <ProductDetails />
       </div>
       <Footer />
     </div>
